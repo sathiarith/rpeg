2150c6330421feb15df25877f08ae90dbcfb9244
sathiarith <sathiarith_chau@uri.edu>
2023-03-31 16:25:25 -0400

    Deleted incorrect ppms and added vidform funct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use rpeg::codec::{compress, decompress};
-use rpeg::utilities::{crop, print_ppm_as_float_array2, print_ppm_as_rgb_array2, rgb_float_to_int, rgb_int_to_float};
+use rpeg::utilities::*;
 extern crate array2;
 use array2::Array2;
 use csc411_image::{Rgb, RgbImage, Read, Write};
@@ -26,17 +26,28 @@ fn main() {
 
     //write out filename to output folder
     let ppm_crop: Array2<Rgb> = crop(&ppm);
-    let ppm_float: Array2<Vec<f64>> = rgb_int_to_float(&ppm_crop, ppm.denominator);
-    print_ppm_as_rgb_array2(&ppm_crop);
-    print_ppm_as_float_array2(&ppm_float);
-    print_ppm_as_rgb_array2(&rgb_float_to_int(&ppm_float, ppm.denominator));
+    let mut ppm_float: Array2<Vec<f64>> = rgb_int_to_float(&ppm_crop, ppm.denominator);
+    //print_ppm_as_rgb_array2(&ppm_crop);
+    //print_ppm_as_float_array2(&ppm_float);
+    //print_ppm_as_rgb_array2(&rgb_float_to_int(&ppm_float, ppm.denominator));
+    let ppm_fcrop = rgb_float_to_int(&ppm_float, ppm.denominator);
     // make new rgbimage with new pixels
     let ppm2 = RgbImage {
         width: ppm_crop.width as u32,
         height: ppm_crop.height as u32,
         denominator: ppm.denominator,
-        pixels: rgb_float_to_int(&ppm_float, ppm.denominator).data,
+        pixels: ppm_fcrop.data,
     };
+    // convert ppm_float to video format
+    rgb_to_vid_form(&mut ppm_float);
+    //iterate through ppm_float and print vector contents
+    for row in 0..ppm_float.height {
+        for col in 0..ppm_float.width {
+            let pixel = ppm_float.get(row,col).unwrap();
+            print!("[Y: {}, Pb: {}, Pr: {}]", pixel[0], pixel[1], pixel[2]);
+        }
+        println!();
+    }
     
     let output_path: &str = output;
     ppm2.write(Some(output_path)).unwrap();
--- src/utilities.rs
+++ src/utilities.rs
@@ -15,11 +15,17 @@ pub fn crop(src: &RgbImage) -> Array2<Rgb> {
     }
 
     let mut dest = Array2::new(crop_width as usize, crop_height as usize, Rgb { red: 0, green: 0, blue: 0 });
+    // print width height of src
+    println!("Width: {}, Height: {}", src.width, src.height);
+    // print width height of dest
+    println!("DWidth: {}, DHeight: {}", dest.width, dest.height);
     let dest_height = dest.height;
     let dest_width = dest.width;
-    for (i, dest_pixel) in dest.iter_row_major().enumerate() {
-        let row = i / dest_height;
-        let col = i % dest_height;
+    println!("Length of data: {}", dest.data.len());
+    for (i, dest_pixel) in dest.iter_row_major_mut().enumerate() {
+        let row = i / dest_width;
+        let col = i % dest_width;
+        println!("Index: {}, Row: {}, Col: {}", row*dest_width+col, row, col);
         *dest_pixel = src.pixels[row * dest_width + col].clone();
     }
     dest
@@ -31,9 +37,8 @@ pub fn print_ppm_as_rgb_array2(ppm_crop: &Array2<Rgb>) {
         println!("Number of Rgb elements in cropped ppm: {}", num_elements);
 
     // print pixel values
-    // UNCERTAIN IF WIDTH AND HEIGHT IS CORRECT
-    for row in 0..ppm_crop.width {
-        for col in 0..ppm_crop.height {
+    for row in 0..ppm_crop.height {
+        for col in 0..ppm_crop.width {
             let pixel = ppm_crop.get(row,col).unwrap();
             print!("[Red: {}, Green: {}, Blue: {}]", pixel.red, pixel.green, pixel.blue);
         }
@@ -47,9 +52,8 @@ pub fn print_ppm_as_float_array2(ppm_crop: &Array2<Vec<f64>>) {
         println!("Number of Rgb elements in cropped ppm: {}", num_elements);
 
     // print pixel values
-    // UNCERTAIN IF WIDTH AND HEIGHT IS CORRECT
-    for row in 0..ppm_crop.width {
-        for col in 0..ppm_crop.height {
+    for row in 0..ppm_crop.height {
+        for col in 0..ppm_crop.width {
             let pixel = ppm_crop.get(row,col).unwrap();
             print!("[Red: {}, Green: {}, Blue: {}]", pixel[0], pixel[1], pixel[2]);
         }
@@ -60,10 +64,10 @@ pub fn print_ppm_as_float_array2(ppm_crop: &Array2<Vec<f64>>) {
 pub fn rgb_int_to_float(ppm_crop: &Array2<Rgb>, denom: u16) -> Array2<Vec<f64>> {
     let mut ppm_float = Array2::new(ppm_crop.width as usize, ppm_crop.height as usize, Vec::new());
     //iterate and change pixels r, g, b values to float
-    let height = ppm_float.height;
-    for (i, dest_pixel) in ppm_float.iter_row_major().enumerate() {
-        let row = i / height;
-        let col = i % height;
+    let width = ppm_float.width;
+    for (i, dest_pixel) in ppm_float.iter_row_major_mut().enumerate() {
+        let row = i / width;
+        let col = i % width;
         let pixel = ppm_crop.get(row, col).unwrap();
         // update pixel values as float to Vec<vec<f64>>
         *dest_pixel = vec![pixel.red as f64 / denom as f64, pixel.green as f64 / denom as f64, pixel.blue as f64 / denom as f64];
@@ -75,14 +79,42 @@ pub fn rgb_int_to_float(ppm_crop: &Array2<Rgb>, denom: u16) -> Array2<Vec<f64>> 
 pub fn rgb_float_to_int(ppm_float: &Array2<Vec<f64>>, denom: u16) -> Array2<Rgb> {
     let mut ppm_int = Array2::new(ppm_float.width as usize, ppm_float.height as usize, Rgb { red: 0, green: 0, blue: 0 });
     //iterate and change pixels r, g, b values to int
-    let height = ppm_int.height;
-    for (i, dest_pixel) in ppm_int.iter_row_major().enumerate() {
-        let row = i / height;
-        let col = i % height;
+    let width = ppm_int.width;
+    for (i, dest_pixel) in ppm_int.iter_row_major_mut().enumerate() {
+        let row = i / width;
+        let col = i % width;
         let pixel = ppm_float.get(row, col).unwrap();
-        // update pixel values as i32 to Vec<vec<f64>>
+        // update pixel values as Vec<vec<f64>> to int
         *dest_pixel = Rgb { red: (pixel[0] * denom as f64) as u16, green: (pixel[1] * denom as f64) as u16, blue: (pixel[2] * denom as f64) as u16 };
     }
 
     ppm_int
+}
+
+pub fn rgb_to_vid_form(ppm_float: &mut Array2<Vec<f64>>) {
+    //iterate and change float values to Y, Pb, Pr
+    for pixel in ppm_float.iter_row_major_mut() {
+        let y = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2];
+        let pb = -0.168736 * pixel[0] - 0.331264 * pixel[1] + 0.5 * pixel[2];
+        let pr = 0.5 * pixel[0] - 0.418688 * pixel[1] - 0.081312 * pixel[2];
+        // update video form pixel values at Vec<vec<f64>> to Y, Pb, Pr
+        pixel[0] = y;
+        pixel[1] = pb;
+        pixel[2] = pr;
+    }
+
+}
+
+pub fn vid_form_to_rgb(ppm_float: &mut Array2<Vec<f64>>) {
+    //iterate and change Y, Pb, Pr values to float
+    for pixel in ppm_float.iter_row_major_mut() {
+        let r = 1.0 * pixel[0] + 0.0 * pixel[1].clone() + 1.402 * pixel[2];
+        let g = 1.0 * pixel[0] - 0.344136 * pixel[1] - 0.714136 * pixel[2];
+        let b = 1.0 * pixel[0] + 1.772 * pixel[1] + 0.0 * pixel[2];
+        // update video form pixel values at Vec<vec<f64>> to R, G, B
+        pixel[0] = r;
+        pixel[1] = g;
+        pixel[2] = b;
+    }
+
 }
