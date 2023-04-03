5ebc26acb6272087e21e392aed48b493030f3168
sathiarith <sathiarith_chau@uri.edu>
2023-04-03 11:24:58 -0400

    2x2 block iteration implemented, needs testing

--- src/utilities.rs
+++ src/utilities.rs
@@ -2,6 +2,15 @@ extern crate array2;
 use array2::Array2;
 use csc411_image::{Rgb, RgbImage};
 
+/// It takes an image, crops it to be even in width and height, and returns the cropped image
+/// 
+/// Arguments:
+/// 
+/// * `src`: The source image to crop
+/// 
+/// Returns:
+/// 
+/// A 2D array of RGB values
 pub fn crop(src: &RgbImage) -> Array2<Rgb> {
     let mut crop_width = src.width;
     let mut crop_height = src.height;
@@ -31,6 +40,11 @@ pub fn crop(src: &RgbImage) -> Array2<Rgb> {
     dest
 }
 
+/// `print_ppm_as_rgb_array2` prints the contents of a 2D array of `Rgb` pixels
+/// 
+/// Arguments:
+/// 
+/// * `ppm_crop`: &Array2<Rgb>
 pub fn print_ppm_as_rgb_array2(ppm_crop: &Array2<Rgb>) {
     let (rows, cols) = (ppm_crop.rows(), ppm_crop.cols());
     let num_elements = rows * cols;
@@ -46,6 +60,11 @@ pub fn print_ppm_as_rgb_array2(ppm_crop: &Array2<Rgb>) {
     }
 }
 
+/// `print_ppm_as_float_array2` prints the contents of a 2D array of RGB values as a series of 3-tuples
+/// 
+/// Arguments:
+/// 
+/// * `ppm_crop`: &Array2<Vec<f64>>
 pub fn print_ppm_as_float_array2(ppm_crop: &Array2<Vec<f64>>) {
     let (rows, cols) = (ppm_crop.rows(), ppm_crop.cols());
     let num_elements = rows * cols;
@@ -61,6 +80,16 @@ pub fn print_ppm_as_float_array2(ppm_crop: &Array2<Vec<f64>>) {
     }
 }
 
+/// It takes a 2D array of RGB values and converts them to a 2D array of floating point values
+/// 
+/// Arguments:
+/// 
+/// * `ppm_crop`: The cropped image
+/// * `denom`: the denominator used to convert the pixel values to float.
+/// 
+/// Returns:
+/// 
+/// A 2D array of Vec<f64>
 pub fn rgb_int_to_float(ppm_crop: &Array2<Rgb>, denom: u16) -> Array2<Vec<f64>> {
     let mut ppm_float = Array2::new(ppm_crop.width as usize, ppm_crop.height as usize, Vec::new());
     //iterate and change pixels r, g, b values to float
@@ -76,6 +105,17 @@ pub fn rgb_int_to_float(ppm_crop: &Array2<Rgb>, denom: u16) -> Array2<Vec<f64>> 
     ppm_float
 }
 
+/// It takes a 2D array of RGB values as floats, and returns a 2D array of RGB values as integers
+/// 
+/// Arguments:
+/// 
+/// * `ppm_float`: The image as a 2D array of RGB values in floating point format.
+/// * `denom`: the denominator to multiply with the float value. For example, if the float value is 0.5, the
+/// product is the original integer value.
+/// 
+/// Returns:
+/// 
+/// A 2D array of RGB values
 pub fn rgb_float_to_int(ppm_float: &Array2<Vec<f64>>, denom: u16) -> Array2<Rgb> {
     let mut ppm_int = Array2::new(ppm_float.width as usize, ppm_float.height as usize, Rgb { red: 0, green: 0, blue: 0 });
     //iterate and change pixels r, g, b values to int
@@ -91,6 +131,12 @@ pub fn rgb_float_to_int(ppm_float: &Array2<Vec<f64>>, denom: u16) -> Array2<Rgb>
     ppm_int
 }
 
+
+/// This function takes a 2D array of RGB values and converts them to Y, Pb, Pr values
+/// 
+/// Arguments:
+/// 
+/// * `ppm_float`: &mut Array2<Vec<f64>>
 pub fn rgb_to_vid_form(ppm_float: &mut Array2<Vec<f64>>) {
     //iterate and change float values to Y, Pb, Pr
     for pixel in ppm_float.iter_row_major_mut() {
@@ -105,6 +151,11 @@ pub fn rgb_to_vid_form(ppm_float: &mut Array2<Vec<f64>>) {
 
 }
 
+/// > This function takes a 2D array of video form pixel values and converts them to RGB pixel values
+/// 
+/// Arguments:
+/// 
+/// * `ppm_float`: &mut Array2<Vec<f64>>
 pub fn vid_form_to_rgb(ppm_float: &mut Array2<Vec<f64>>) {
     //iterate and change Y, Pb, Pr values to float
     for pixel in ppm_float.iter_row_major_mut() {
@@ -119,6 +170,16 @@ pub fn vid_form_to_rgb(ppm_float: &mut Array2<Vec<f64>>) {
 
 }
 
+/// `array2rgb_to_rgbimg` takes an `Array2<Rgb>` and a `u16` and returns an `RgbImage`
+/// 
+/// Arguments:
+/// 
+/// * `ppm_src`: The source image to be converted.
+/// * `denom`: the denominator of the RGB values.
+/// 
+/// Returns:
+/// 
+/// A RgbImage struct.
 pub fn array2rgb_to_rgbimg(ppm_src: &Array2<Rgb>, denom: u16) -> RgbImage {
 
     let rgb = RgbImage {
@@ -128,4 +189,30 @@ pub fn array2rgb_to_rgbimg(ppm_src: &Array2<Rgb>, denom: u16) -> RgbImage {
         pixels: ppm_src.data.clone(),
     };
     rgb
+}
+
+pub fn vid_form_to_cos_transform(ppm_video_float: &Array2<Vec<f64>>) -> Array2<Vec<f64>> {
+    let mut ppm_cos_transform = Array2::new(ppm_video_float.width as usize, ppm_video_float.height as usize, Vec::new());
+    // cosine transform
+    let width = ppm_video_float.width;
+    let height = ppm_video_float.height;
+
+    // iterate through ppm_cost_transform to create a, b, c, d , avg_Pb, and avg_Pr values
+    // based on the 2x2 block of pixels from ppm_video_float
+     // Check sub-grids
+    // Row_start and col_start loops are the coordinates of the top-left corner of the subgrid.
+    for row_start in (0..height).step_by(2) {
+        for col_start in (0..width).step_by(2) {
+            
+            // Row_offset and col_offset loops indices the subgrid.
+            for row_offset in 0..2 {
+                for col_offset in 0..2 {
+                    let ppm_src = ppm_video_float.get(row_start + row_offset, col_start + col_offset).unwrap();
+                    let ppm_dest = ppm_cos_transform.get_mut(row_start + row_offset, col_start + col_offset).unwrap();
+                }
+            }
+        }
+    }
+
+    ppm_cos_transform
 }
